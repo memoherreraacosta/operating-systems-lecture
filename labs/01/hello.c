ae2382e783d34168d904f61d04bac64b094989cb
memoherreraacosta <memoherreraacosta@users.noreply.github.com>
2018-05-01 18:30:43 -0500

    Update hello.c

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -4,6 +4,6 @@
 
 int main(void) {
     printf("Hello World\n");
-    printf("My name is Christopher Salvador Márquez Álvarez \n");
+    printf("My name is Guillermo Herrera Acosta \n");
     return 0; 
 }
